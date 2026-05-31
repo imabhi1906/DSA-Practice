23ba13412189b62d7ac15e22cb88a950a961460a
abhishek <sayabhi2002@gmail.com>
2026-06-01 01:58:24 +0530

    palindrome

--- 00 Extraction of Digits.py
+++ 00 Extraction of Digits.py
@@ -1,3 +1,10 @@
+def printDigitsRev(n):
+    li = []
+    while n>0:
+        li.append(n%10)
+        n = n//10
+    return li
+
 def countDigits(n):
     i=0
     while n>0:
@@ -6,14 +13,39 @@ def countDigits(n):
     return i
 
 
-def printDigitsRev(n):
-    li = []
+def revDigit(n):
+    res = 0
     while n>0:
-        li.append(n%10)
-        n = n//10
-    return li
+        rem = n%10
+        res = res*10+rem
+        n=n//10
+    return res
 
+def palindromeCheck(n):
+    if (n==revDigit(n)):
+        return True
+    else:
+        return False
 
 number = int(input("Enter a number: "))
-# print(printDigitsRev(number))
-print(countDigits(number))
+
+choice = int(input(
+    "Enter your choice:\n"
+    "1) Print array of digits reversed\n"
+    "2) Count digits\n"
+    "3) Reverse digits\n"
+    "4) Palindrome check\n"
+    ":"
+))
+
+match choice:
+    case 1:
+        print(printDigitsRev(number))
+    case 2:
+        print(countDigits(number))
+    case 3:
+        print(revDigit(number))
+    case 4:
+        print(palindromeCheck(number))
+    case _:
+        print("Invalid choice")
