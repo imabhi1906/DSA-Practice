199da7cdd72ee6cfdb5d479c1369cc787fe3f7be
abhishek <sayabhi2002@gmail.com>
2026-07-28 13:01:39 +0530

    updated after interview

---
+++ Interview question/dictionary.py
@@ -0,0 +1,60 @@
+dictionary = { "abhishek" : 1, "anil": 2}
+
+# # dictionary["abhishek"] = 2
+
+# # for k in dictionary:
+# #     if dictionary[2] == 2:
+# #         k = "Abhi"
+
+# print(dictionary)
+
+# dictionary = { "abhishek" : 1, "anil": 2, "hello": 3}
+
+
+# print(dictionary)
+
+# string = "abhishek"
+
+# print(string)
+# string = string+" Anil"
+
+# print(string)
+
+
+
+
+
+
+li = []
+
+# string = input("enter String:")
+string = "Hello"
+li.append(string)
+
+# integer = int(input("Enter integer: "))
+integer = 7
+li.append(integer)
+
+tupleList = (11,22,33,44)
+li.append(tupleList)
+
+li.append(dictionary)
+
+li.append(3.14)
+
+
+print(li)
+
+# li[2], li["abhishek"] = li[3["abhishek"]], li[2]
+
+# print(li)
+    
+
+
+
+
+
+
+
+
+
